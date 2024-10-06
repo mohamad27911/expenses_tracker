12f7b5e7a5fab477a0e0bf74218cd9f8727e874e
mohamad27911 <mohamad.mar72@gmail.com>
2024-10-06 14:57:16 +0300

    applied react hook form to validate data

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,103 +1,92 @@
-import { useState } from "react";
+
+import { FieldValues, useForm } from "react-hook-form";
 
 function Form() {
-  const [transactions, setTransactions] = useState([
-    {
-      date: "2023-04-01",
-      category: "income",
-      amount: 5000,
-      description: "Salary",
-    },
-  ]);
 
-  const [newTransaction, setNewTransaction] = useState({
-    date: "",
-    category: "",
-    amount: 0,
-    description: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset
+  } = useForm();
 
-  const handleInputChange = (e) => {
-    setNewTransaction({
-      ...newTransaction,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleAddTransaction = () => {
-    setTransactions([...transactions, newTransaction]);
-    setNewTransaction({
-      date: "",
-      category: "",
-      amount: 0,
-      description: "",
-    });
-  };
+  const onSubmit = async (data: FieldValues) => {
+    await new Promise((res) => setTimeout(res, 1000))
+    reset();
+    console.log(data);
+  }
 
   return (
     <div className="mt-8 mx-6 text-white">
       <h2 className="text-3xl font-bold mb-4 text-white">Add New Transaction</h2>
-      <form className="grid md:grid-cols-4 grid-cols-2 gap-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="grid md:grid-cols-4 grid-cols-2 gap-4">
         <div>
           <label htmlFor="date" className="block mb-2">
             Date
           </label>
           <input
+            {...register("date", {
+              required: "Please enter a date"
+            }
+            )}
             type="date"
             id="date"
             name="date"
-            value={newTransaction.date}
-            onChange={handleInputChange}
             className="bg-secondary rounded px-4 py-2 w-full"
           />
+          {errors.date &&<span className="text-expense">{`${errors.date.message}`}</span>}
         </div>
         <div>
           <label htmlFor="category" className="block mb-2">
             Category
           </label>
           <select
+            {...register("category", { required: "Please Enter Category" })}
             id="category"
             name="category"
-            value={newTransaction.category}
-            onChange={handleInputChange}
             className="bg-secondary rounded px-4 py-2 w-full"
           >
             <option value="">Select a category</option>
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </select>
+          {errors.category &&<span className="text-expense">{`${errors.category.message}`}</span>}
         </div>
         <div>
           <label htmlFor="amount" className="block mb-2">
             Amount
           </label>
           <input
+            {...register("amount", { required: "Please Enter Amount", min: {
+              value: 0.01, 
+              message: "Amount must be a positive number" 
+            } })}
             type="number"
             id="amount"
             name="amount"
-            value={newTransaction.amount}
-            onChange={handleInputChange}
             className="bg-secondary rounded px-4 py-2 w-full"
           />
+          {errors.amount &&<span className="text-expense">{`${errors.amount.message}`}</span>}
         </div>
         <div>
           <label htmlFor="description" className="block mb-2">
             Description
           </label>
           <input
+            {...register("description", { required: "Please Enter A Description" })}
             type="text"
             id="description"
             name="description"
-            value={newTransaction.description}
-            onChange={handleInputChange}
             className="bg-secondary rounded px-4 py-2 w-full text-white"
           />
+          {errors.description &&<span className="text-expense">{`${errors.description.message}`}</span>}
+
         </div>
         <div className="col-span-2 md:col-span-4 text-right">
           <button
-            type="button"
-            onClick={handleAddTransaction}
-            className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded"
+            disabled={isSubmitting}
+            type="submit"
+            className="bg-text-light hover:scale-110 transition-transform  text-white font-bold py-2 px-4 rounded"
           >
             Add Transaction
           </button>
